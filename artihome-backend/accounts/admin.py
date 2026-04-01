ec180d6dd952da69570a4b0c02d8e5e241a6611d
Tarun12082005 <venkattarun962@gmail.com>
2026-04-01 22:05:35 +0530

    Fix custom user admin add form

--- artihome-backend/accounts/admin.py
+++ artihome-backend/accounts/admin.py
@@ -1,13 +1,46 @@
+from django import forms
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+
 from .models import User
 
-class UserAdmin(BaseUserAdmin):
-    list_display = ('email', 'first_name', 'last_name', 'city', 'phone', 'is_staff', 'created_at')
-    search_fields = ('email', 'first_name', 'last_name', 'phone')
-    ordering = ('email',)
-    list_filter = ('is_staff', 'is_active', 'city')
-    filter_horizontal = ()
-    fieldsets = ()
 
-admin.site.register(User, UserAdmin)
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ('email', 'first_name', 'last_name', 'phone', 'city')
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = User
+        fields = '__all__'
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+    list_display = ('email', 'first_name', 'last_name', 'city', 'phone', 'is_staff', 'is_superuser', 'created_at')
+    search_fields = ('email', 'first_name', 'last_name', 'phone', 'city')
+    ordering = ('email',)
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'city')
+    filter_horizontal = ('groups', 'user_permissions')
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'phone', 'city')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'created_at')}),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                'classes': ('wide',),
+                'fields': ('email', 'first_name', 'last_name', 'phone', 'city', 'password1', 'password2'),
+            },
+        ),
+    )
